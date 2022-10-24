282e1bf80c63f55023d559268a16e9e1b19aecb6
Anton Rusev <anton.rusev.code@gmail.com>
2022-10-24 12:10:35 +0300

    fix: deploy script

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1,25 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
-import "hardhat/console.sol";
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IBridge.sol";
 import "./CrossChain.sol";
-import "./interfaces/IERC20.sol";
-import "./LiquidityManager.sol";
+import "./LiquidityPool.sol";
 
 contract Bridge is IBridge, CrossChain {
-    mapping(address => mapping(address => uint256)) public withdrawable;
-
     // address of the liquidity pool
-    LiquidityManager public liquidityPool;
+    LiquidityPool public liquidityPool;
 
     event BridgedTokenWithdrawn(uint amount);
 
     error NoBridgeTokenToWithdraw();
 
     constructor(address _tunnel, address _liquidityPool) CrossChain(_tunnel) {
-        liquidityPool = LiquidityManager(liquidityPool);
+        liquidityPool = LiquidityPool(liquidityPool);
     }
 
     function _unlockBridgedTokenRequest(
@@ -38,7 +34,7 @@ contract Bridge is IBridge, CrossChain {
 
     function bridgeToken(address token, uint256 amount) external {
         // Deposit ERC20 to the bridge / LP
-
+        
         _sendMessage(_unlockBridgedTokenRequest(token, msg.sender, amount));
     }
 
