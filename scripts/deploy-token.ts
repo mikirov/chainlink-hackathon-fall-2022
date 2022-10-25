da5361b1f120c614688a2190a6b29a7bb7e7b988
Mihail Kirov <shimonchick@gmail.com>
2022-10-25 18:04:11 +0300

    fix: remove nonce from deploy token script

--- scripts/deploy-token.ts
+++ scripts/deploy-token.ts
@@ -4,7 +4,7 @@ export async function deployToken() {
     const [ethereumSigner] = await ethers.getSigners();
     const ownerAddress = await ethereumSigner.getAddress();
 	const tokenFactory = await ethers.getContractFactory("TestBridgedToken", ethereumSigner);
-	const token = await tokenFactory.deploy("TestBridgedToken", "TST1", ownerAddress, {nonce: 79});
+	const token = await tokenFactory.deploy("TestBridgedToken", "TST1", ownerAddress);
     await token.deployed();
 
     await token.mint(ownerAddress, ethers.utils.parseEther("100000", "ether"));
